38e1999161929f995faf59c3462604ffad2f797c
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-02-04 19:09:02 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1 ; i<=n ; i++)     {         for(j=1 ; j<=i ; j++)         {             printf("%d",j);
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,c;
-    scanf("%d" , &n);
-    for(r=1 ;r<=n ; r++)
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1 ; i<=n ; i++)
     {
-        for(c=1 ; c<=n ; c++)
+        for(j=1 ; j<=i ; j++)
         {
-            printf("%c ",r+64);
+            printf("%d",j);
+            
         }
         printf("
 ");
