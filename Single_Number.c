82a11248af2d3c9460a752a684cdcd7e29389c4c
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-03-01 12:31:20 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n],i,j,c=0;     for(i=0 ; i<n; i++)     {         scanf("%d",arr+i);     }     for(i=0 ; i<n; i++)     {         c=0;         for(j=0 ;j<n ; j++)         {             if(arr[i]==arr[j])             {                 c++;             }         }         if(c==1)         {             printf("%d ",arr[i]);         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n],i,j,c=0;
+    for(i=0 ; i<n; i++)
+    {
+        scanf("%d",arr+i);
+    }
+    for(i=0 ; i<n; i++)
+    {
+        c=0;
+        for(j=0 ;j<n ; j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                c++;
+            }
+        }
+        if(c==1)
+        {
+            printf("%d ",arr[i]);
+        }
+    }
+}
