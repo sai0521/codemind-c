2707d0699492b635ee558792f42d2136c06cc5bf
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-05-15 09:59:15 +0530

    #include<stdio.h> int prime(int n) {     int c=0,i;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return n;     }     else     {         return 0;     } } int main() {     int n,i,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",arr+i);     }     for(i=0;i<n;i++)     {         if(prime(arr[i]))         {             c++;         }     }     printf("%d",c); }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int c=0,i;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return n;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",arr+i);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(prime(arr[i]))
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
