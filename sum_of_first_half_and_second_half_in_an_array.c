05b7386039b939ecae242789f91ee33c127c5848
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-05-17 21:18:38 +0530

    #include<stdio.h> int main() {     int n,i,k,s=0,a=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",arr+i);     }     k=n/2;     for(i=0;i<k;i++)     {         s=s+arr[i];     }     for(i=k;i<n;i++)     {         a=a+arr[i];     }     printf("%d ",s);     printf("%d",a); }

---
+++ sum_of_first_half_and_second_half_in_an_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,k,s=0,a=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",arr+i);
+    }
+    k=n/2;
+    for(i=0;i<k;i++)
+    {
+        s=s+arr[i];
+    }
+    for(i=k;i<n;i++)
+    {
+        a=a+arr[i];
+    }
+    printf("%d
+",s);
+    printf("%d",a);
+}
