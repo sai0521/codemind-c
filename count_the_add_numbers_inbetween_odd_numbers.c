4ccc42a03590d075c1c68357ac95c8d0aefcdb04
sai0521 <97425747+sai0521@users.noreply.github.com>
2022-06-19 21:46:23 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",arr+i);     }     int c=0;     for(i=1;i<n-1;i++)     {         if(arr[i-1]%2!=0 && arr[i+1]%2!=0)         {             if(arr[i]%2!=0)             {                 c++;             }         }
        }
        printf("%d",c);
    }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",arr+i);
+    }
+    int c=0;
+    for(i=1;i<n-1;i++)
+    {
+        if(arr[i-1]%2!=0 && arr[i+1]%2!=0)
+        {
+            if(arr[i]%2!=0)
+            {
+                c++;
+            }
+        }
+       
+    }
+    printf("%d",c);
+}
